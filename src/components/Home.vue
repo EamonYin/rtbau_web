1393956f7d148a7d81ea6e5f8b643bac825337cc
EamonYin <xiaominglovejava@163.com>
2022-03-27 20:18:24 +0800

    前端一些探索性的初学

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="add">
+    <h1>欢迎进入EamonPlanet！</h1>
+    <select v-model="mybox">
+      <option v-for="item in list" :value="item">{{ item }}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default{
+    name: "Home",
+    data(){
+        return{
+            mybox:'',
+            list:['北京','上海','深圳']
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
+
--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
+import ElementUI from 'element-ui'   
+import 'element-ui/lib/theme-chalk/index.css'   //引用 element-ui 样式
 
+Vue.use(ElementUI )  //最后要use ElementUI ，不然会报错
+Vue.prototype.$http=axios;//修改内部的$http为axios $http.get("") .post()
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
