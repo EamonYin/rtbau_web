0989a5f4167602f113203f7e3967d0ae6fdf44b5
EamonYin <xiaominglovejava@163.com>
2022-03-28 20:51:12 +0800

    修改为el-select和el-option

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,14 @@
 <template>
   <div id="add">
     <h1>欢迎进入EamonPlanet！</h1>
-    <select v-model="mybox">
-      <option v-for="item in list" :value="item">
-        {{ item.regionName }}
-        <div v-show="false" v-text="item.regionCode"></div>
-        </option>
-    </select>
+    <el-select v-model="mybox" placeholder="请选择地区">
+      <el-option v-for="item in list" key="label" :value="item.regionCode" :label="item.regionName">
+        <!-- {{ item.regionName }}
+        <div v-show="false" v-text="item.regionCode"></div> -->
+      </el-option>
+    </el-select>
+    <br>
+    <!-- <span>您选择的地区为：{{ mybox }}</span> -->
   </div>
 </template>
 
